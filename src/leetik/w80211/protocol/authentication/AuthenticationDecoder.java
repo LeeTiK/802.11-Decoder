b6f4b47b0072ebf42744c7ebd2510fe48b4a3c72
L33TiK <val1994gg@yandex.ru>
2020-03-06 18:43:58 +0300

    add protocol Authentication (EAPOL 4-way handshake)

---
+++ src/leetik/w80211/protocol/authentication/AuthenticationDecoder.java
@@ -0,0 +1,161 @@
+package leetik.w80211.protocol.authentication;
+
+import leetik.w80211.protocol.wlan.frame.management.element.EWlanElementID;
+import leetik.w80211.protocol.wlan.frame.management.element.IWlanElement;
+import leetik.w80211.protocol.wlan.frame.management.element.WlanElementAbstr;
+import leetik.w80211.protocol.wlan.frame.management.element.WlanElementIdDecoder;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.List;
+
+public class AuthenticationDecoder {
+
+    byte version;
+
+    byte type;
+
+    short length;
+
+    byte keyDescriptorType;
+
+    KeyInformation keyInformation;
+
+    short keyLength;
+
+    long replayCounter;
+
+    byte[] wpaKeyNonce;
+    byte[] keyIV;
+    byte[] wpaKeyRsc;
+    byte[] wpaKeyID;
+    byte[] wpaKeyMic;
+
+    short wpaKeyDataLength;
+
+    List<IWlanElement> taggedParameter;
+
+    public AuthenticationDecoder(ByteBuffer byteBuffer)
+    {
+        version = byteBuffer.get();
+        type = byteBuffer.get();
+        length = byteBuffer.getShort();
+
+        keyDescriptorType = byteBuffer.get();
+        keyInformation = new KeyInformation(byteBuffer);
+
+        keyLength = byteBuffer.getShort();
+
+        replayCounter = byteBuffer.getLong();
+
+        wpaKeyNonce = new byte[32];
+        byteBuffer.get(wpaKeyNonce);
+
+        keyIV = new byte[16];
+        byteBuffer.get(keyIV);
+
+        wpaKeyRsc = new byte[8];
+        byteBuffer.get(wpaKeyRsc);
+        wpaKeyID = new byte[8];
+        byteBuffer.get(wpaKeyID);
+
+        wpaKeyMic = new byte[16];
+        byteBuffer.get(wpaKeyMic);
+
+        wpaKeyDataLength = byteBuffer.getShort();
+
+        taggedParameter = WlanElementIdDecoder.decode(byteBuffer);
+    }
+
+    public byte getKeyDescriptorType() {
+        return keyDescriptorType;
+    }
+
+    public byte getType() {
+        return type;
+    }
+
+    public byte getVersion() {
+        return version;
+    }
+
+    public byte[] getKeyIV() {
+        return keyIV;
+    }
+
+    public byte[] getWpaKeyID() {
+        return wpaKeyID;
+    }
+
+    public byte[] getWpaKeyMic() {
+        return wpaKeyMic;
+    }
+
+    public byte[] getWpaKeyNonce() {
+        return wpaKeyNonce;
+    }
+
+    public byte[] getWpaKeyRsc() {
+        return wpaKeyRsc;
+    }
+
+    public KeyInformation getKeyInformation() {
+        return keyInformation;
+    }
+
+    public List<IWlanElement> getTaggedParameter() {
+        return taggedParameter;
+    }
+
+    public long getReplayCounter() {
+        return replayCounter;
+    }
+
+    public short getKeyLength() {
+        return keyLength;
+    }
+
+    public short getLength() {
+        return length;
+    }
+
+    public short getWpaKeyDataLength() {
+        return wpaKeyDataLength;
+    }
+
+
+    public WlanElementAbstr getTaggedParameter(EWlanElementID EWlanElementID){
+        if (taggedParameter==null) return null;
+
+        for (int i=0; i<taggedParameter.size(); i++)
+        {
+            if (taggedParameter.get(i).getWlanElementId()== EWlanElementID) return (WlanElementAbstr) taggedParameter.get(i);
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        AuthenticationDecoder authenticationDecoder = (AuthenticationDecoder) obj;
+
+        if (authenticationDecoder.getKeyInformation().getMessageNumber()!=this.getKeyInformation().getMessageNumber()) {
+            return false;
+        }
+
+        if (Arrays.equals(wpaKeyNonce,authenticationDecoder.wpaKeyNonce)){
+            return true;
+        }
+        else return false;
+
+       // return false;
+    }
+
+}
---
+++ src/leetik/w80211/protocol/authentication/KeyInformation.java
@@ -0,0 +1,156 @@
+package leetik.w80211.protocol.authentication;
+
+import java.nio.ByteBuffer;
+
+public class KeyInformation {
+    short keyinfo =0x00;
+
+    byte keyDescriptionVersion = 0x00;
+    byte keyType =0x00;
+    byte keyIndex = 0x00;
+    boolean install =false;
+    boolean keyAck = false;
+    boolean ketMic = false;
+    boolean secure = false;
+    boolean error = false;
+    boolean request = false;
+    boolean encryptedKeyData = false;
+    boolean smkMessage = false;
+
+    byte messageNumber = 0;
+
+    public KeyInformation(ByteBuffer byteBuffer)
+    {
+        short keyinfo = byteBuffer.getShort();
+        decoder(keyinfo);
+    }
+
+    public KeyInformation(short keyinfo)
+    {
+        decoder(keyinfo);
+    }
+
+    private void decoder(short keyinfo){
+        this.keyinfo = keyinfo;
+
+        keyDescriptionVersion = (byte) (keyinfo & 0x0007);
+        keyType = (byte) ((keyinfo & 0x0008) >> 3);
+        keyIndex = (byte) ((keyinfo & 0x0030) >> 4);
+
+        if ((keyinfo & 0x0040) !=0)
+        {
+            install = true;
+        }
+
+        if ((keyinfo & 0x0080) !=0)
+        {
+            keyAck = true;
+        }
+
+        if ((keyinfo & 0x0100) !=0)
+        {
+            ketMic = true;
+        }
+
+        if ((keyinfo & 0x0200) !=0)
+        {
+            secure = true;
+        }
+
+        if ((keyinfo & 0x0400) !=0)
+        {
+            error = true;
+        }
+
+        if ((keyinfo & 0x0800) !=0)
+        {
+            request = true;
+        }
+
+        if ((keyinfo & 0x1000) !=0)
+        {
+            encryptedKeyData = true;
+        }
+
+        if ((keyinfo & 0x2000) !=0)
+        {
+            smkMessage = true;
+        }
+
+        calculatedNumberMessage();
+    }
+
+    private void calculatedNumberMessage() {
+        if (!isInstall() && isKeyAck() && !isKetMic() && !isSecure())
+        {
+            messageNumber = 1;
+        } else {
+            if (!isInstall() && !isKeyAck() && isKetMic() && !isSecure())
+            {
+                messageNumber = 2;
+            } else {
+                if (isInstall() && isKeyAck() && isKetMic() && isSecure())
+                {
+                    messageNumber = 3;
+                } else {
+                    if (!isInstall() && !isKeyAck() && isKetMic() && isSecure())
+                    {
+                        messageNumber = 4;
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean isEncryptedKeyData() {
+        return encryptedKeyData;
+    }
+
+    public boolean isError() {
+        return error;
+    }
+
+    public boolean isInstall() {
+        return install;
+    }
+
+    public boolean isKetMic() {
+        return ketMic;
+    }
+
+    public boolean isKeyAck() {
+        return keyAck;
+    }
+
+    public boolean isRequest() {
+        return request;
+    }
+
+    public boolean isSecure() {
+        return secure;
+    }
+
+    public boolean isSmkMessage() {
+        return smkMessage;
+    }
+
+    public byte getKeyDescriptionVersion() {
+        return keyDescriptionVersion;
+    }
+
+    public byte getKeyIndex() {
+        return keyIndex;
+    }
+
+    public byte getKeyType() {
+        return keyType;
+    }
+
+    public byte getMessageNumber() {
+        return messageNumber;
+    }
+
+    public short getKeyinfo() {
+        return keyinfo;
+    }
+}
