60f402789b5cc848976020fb40d1c55fc8212077
L33TiK <val1994gg@yandex.ru>
2020-03-17 13:42:40 +0300

    add check PMKID empty

--- src/leetik/w80211/protocol/wlan/frame/management/element/impl/vendorSpecific/RsnPmkid.java
+++ src/leetik/w80211/protocol/wlan/frame/management/element/impl/vendorSpecific/RsnPmkid.java
@@ -20,6 +20,22 @@ public class RsnPmkid {
         return PMKID;
     }
 
+    public boolean isValidPMKID(){
+        if (PMKID==null) return false;
+
+        boolean checkBAD = true;
+        for (int i=0; i<PMKID.length; i++){
+            if (PMKID[i]!=0x00){
+                checkBAD = false;
+                break;
+            }
+        }
+
+        if (checkBAD) return false;
+
+        return true;
+    }
+
     @Override
     public String toString(){
         return HexDump.toHexString(PMKID);
